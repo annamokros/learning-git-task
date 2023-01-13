a3ad56aa6765b65e5723e61ed34e1f2ad921f132
Anna Mokros <annamokros@gmail.com>
2023-01-13 13:33:09 +0100

    Create for loop

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,5 @@ shopping_dictionary ={
     "piekarnia" : ['chleb', 'pączek', 'bułki'],
     "warzywniak" : ['marchew', 'seler', 'rukola']
 }
+for shop, food in shopping_dictionary.items():
+    print(f"Idę do {shop.title()}, kupuję tu następujące rzeczy: {str(food).title()}")
