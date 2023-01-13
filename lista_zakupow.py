9dbcc20cea5c68ae690d51be0171632f48c4b8ad
Anna Mokros <annamokros@gmail.com>
2023-01-13 18:17:02 +0100

    New loop for counting food in dictionary

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,8 +1,10 @@
+count = 0
 shopping_dictionary ={
     "piekarnia" : ['chleb', 'pączek', 'bułki'],
     "warzywniak" : ['marchew', 'seler', 'rukola']
 }
 for shop, food in shopping_dictionary.items():
     print(f"Idę do {shop.title()}, kupuję tu następujące rzeczy: {str(food).title()}")
-shopping_cart = len(food)*2
-print(f"W sumie kupuję {shopping_cart} produktów")
+    for i in food:
+        count +=1
+print(f"W sumie kupuję {count} produktów")
