160e0a8550e157a96c34ebcf00fe6e4348615c28
Anna Mokros <annamokros@gmail.com>
2023-01-13 13:36:21 +0100

    Create shopping summary

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -4,3 +4,5 @@ shopping_dictionary ={
 }
 for shop, food in shopping_dictionary.items():
     print(f"Idę do {shop.title()}, kupuję tu następujące rzeczy: {str(food).title()}")
+shopping_cart = len(food)*2
+print(f"W sumie kupuję {shopping_cart} produktów")
